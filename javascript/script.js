9de5750a033ee5274cbf33710af848aeab0b62b8
Federica Pozzer <federica.pozzer@gmail.com>
2023-01-20 15:29:32 +0100

    final commit!

--- javascript/script.js
+++ javascript/script.js
@@ -1,8 +1,16 @@
+// CHIEDO INFO
 let user_name = prompt("come ti chiami?");
 let user_surname = prompt("quale è il tuo cognome?");
 let favourite_color = prompt("quale è il tuo colore preferito?");
 
+    // le stampo in console per vedere se è tutto ok - OK!
 console.log(`${user_name} ${user_surname} ${favourite_color}`);
 
-const user_password = (user_name + user_surname + favourite_color + "23");
+// CALCOLO PASSWORD
+const user_password = `${user_name}${user_surname}${favourite_color}23`;
 
+    // stampo in console per vedere se è tutto ok - OK!
+console.log(user_password);
+
+// ok, vediamo se funziona ---> YESSS!
+document.getElementById("password").innerHTML = user_password;
