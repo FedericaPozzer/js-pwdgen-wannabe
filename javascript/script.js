8b10afc2cf645cdb4bcd3ceb5c53c1cc6cd6ca5c
Federica Pozzer <federica.pozzer@gmail.com>
2023-01-23 10:24:20 +0100

    last commit

--- javascript/script.js
+++ javascript/script.js
@@ -6,8 +6,15 @@ let favourite_color = prompt("quale è il tuo colore preferito?");
     // le stampo in console per vedere se è tutto ok - OK!
 console.log(`${user_name} ${user_surname} ${favourite_color}`);
 
-// CALCOLO PASSWORD - prima funzionava anche senza parentesi tonde, ma quello sotto (quello del bonus) no, e allora le ho aggiunte anche qui!
-const user_password = (`${user_name}${user_surname}${favourite_color}23`);
+// CALCOLO PASSWORD
+const user_password = `${user_name}${user_surname}${favourite_color}23`;
+// si poteva scrivere anche così:
+//  const user_password = user_name + user_surname + favourite_color + "23";
+// e anche così:
+// const user_password = la password è: ${user_name}${user_surname}${favourite_color};
+// ma il metodo usato (che si chiama "literal template" è il migliore!)
+// andava bene anche (sempre literal):
+// const user_password = `${user_name + user_surname ecc}` che funziona ma si usa più che altro per fare somme, se no è meglio scriverle separate (ogniuna nella sua $graffa)
 
     // stampo in console per vedere se è tutto ok - OK!
 console.log(user_password);
@@ -23,7 +30,7 @@ let favourite_number = prompt("quale è il tuo numero preferito?")
     // stampo in console - OK!
 console.log(`${favourite_number}`);
     // creo il bonus
-const bonus_password = (`${user_name}${user_surname}${favourite_color}${favourite_number}`);
+const bonus_password = `${user_name}${user_surname}${favourite_color}${favourite_number}`;
     // funziona? - SI!
 console.log(bonus_password);
     // stampo in HTML
@@ -33,7 +40,7 @@ document.getElementById("bonus-1").innerHTML = bonus_password;
     // generare un numero random -ok!
 document.getElementById("bonus-random-number").innerHTML = Math.floor(Math.random() * 100);
     // e poi??
-const bonus_password_two = (`${user_name}${user_surname}${favourite_color}${Math.floor(Math.random() * 100)}`);
+const bonus_password_two = `${user_name}${user_surname}${favourite_color}${Math.floor(Math.random() * 100)}`;
 console.log(bonus_password_two);
     //stampo in HTML - yeeee!
 document.getElementById("bonus-2").innerHTML = bonus_password_two;
